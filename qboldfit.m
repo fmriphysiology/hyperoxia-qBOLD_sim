4c91126b01cefddcde97cbaa912a0e8b5ac3693a
Nic Blockley <nicholas.blockley@ndcn.ox.ac.uk>
2016-03-17 18:11:08 +0000

    Calculate R2p, CBV and OEF

--- qboldfit.m
+++ qboldfit.m
@@ -1,6 +1,8 @@
-function sig_ase=plotresults(p,storedPhase,TE);
+function [vmeas r2pmeas e0meas]=qBOLDfit(p,storedPhase,TE)
 
 	t=(p.deltaTE:p.deltaTE:p.TE*2)';
+	deltaChi0=0.264e-6;
+	Hct=0.4;
 	
 	if nargin>2
 		p.TE=TE;
@@ -20,24 +22,15 @@ function sig_ase=plotresults(p,storedPhase,TE);
 		ASEPhase(k,:)=sum(storedPhase(1:k-1,:),1)-sum(storedPhase(k:TEind,:),1);
 	end
 	tau_ase=(p.TE:-p.deltaTE*2:-p.TE)';
-	sig_ase=abs(sum(exp(-i.*ASEPhase),2)./p.N);
+	sig_ase=abs(sum(exp(-i.*ASEPhase(:,find(p.numStepsInVessel==0))),2)./sum(p.numStepsInVessel==0));
+	sum(p.numStepsInVessel==0)
 	
-	%generate a GESSE weighted signal
-	TE2ind=find(round(t.*1000)==round(p.TE*500),1,'first');
-	mask=repmat([ones(TE2ind,1); -ones(size(storedPhase,1)-TE2ind,1)],1,p.N);
-	GESSEPhase=cumsum(storedPhase.*mask,1);
-	tau_gesse=t-p.TE;
-	sig_gesse=abs(sum(exp(-i.*GESSEPhase),2)./p.N);
+	tau_ind=find(tau_ase>15e-3);
+	X=[ones(size(tau_ind)) -tau_ase(tau_ind)];
+	a=X\log(sig_ase(tau_ind));
 	
-	%plot signal curves
-	figure(100);
-	hold on;
-	plot(tau_gesse.*1000,sig_gesse,'o-');
-	box on;
+	r2pmeas=a(2);
+	vmeas=a(1)-log(sig_ase(find(tau_ase==0)));
+	e0meas=(r2pmeas./vmeas)./(4/3*pi*p.gamma.*p.B0.*deltaChi0.*Hct);
 	
-	figure(101);
-	hold on;
-	plot(tau_ase.*1000,sig_ase,'o-');
-	box on;
-
-return;
+	%keyboard;
